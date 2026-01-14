fd14970478424eedac575439ff799e0e244572b2
nrb2002 <tshibasubaron@yahoo.fr>
2026-01-14 22:18:00 +0100

    dotenv installed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const { connectDB } = require("./db/mongo");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors()); //controls origin access
 app.use(express.json());
 
 connectDB().then(() => {
