1da3da9de0ac78bcc4678fb541a4388020cb5361
nrb2002 <tshibasubaron@yahoo.fr>
2026-01-16 16:27:27 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req,res) => {
-    res.send('Welcome to CSE 341 - Project 1!');
+    res.send('Welcome to Project 1!');
 });
 
 module.exports = router;
