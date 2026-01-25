537f4dcca5487ce69a80d91c1e0cfcd0d23d86f2
nrb2002 <tshibasubaron@yahoo.fr>
2026-01-26 00:47:30 +0100

    Swagger configuration fixed

--- src/swagger.js
+++ src/swagger.js
@@ -8,8 +8,9 @@ const doc = {
         version: "1.0.0",
         description: 'This is an API for storing and retrieving information about contacts. These contacts are to be stored in a MongoDB database and all interaction happen through the API. The API can be used by any frontend.',
     },
-    host: 'https://cse341-project1-baron.onrender.com',
-    schemes: ['https']
+    host: process.env.NODE_ENV === 'production' ? 'cse341-project1-baron.onrender.com' : 'localhost:3000',
+    schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http']
+
 };
 
 const outputFile = './swagger.json';
