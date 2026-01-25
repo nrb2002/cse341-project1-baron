ba609e76d3a027a9b61b0c244ad3c71004461d22
nrb2002 <tshibasubaron@yahoo.fr>
2026-01-25 23:39:56 +0100

    Update swagger.js

--- src/swagger.js
+++ src/swagger.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const swaggerAutogen = require('swagger-autogen')(); //import swagger package
 
 //Build the documentation
 
 const doc = {
     info: {
-        title: 'Contacts API',
+        title: 'Contacts API by Baron',
         version: "1.0.0",
         description: 'This is an API for storing and retrieving information about contacts. These contacts are to be stored in a MongoDB database and all interaction happen through the API. The API can be used by any frontend.',
     },
